afd85862a956ef179aeedb227b74b89293adfff4
Tohid Khan <tohidkhan9050482152@gmail.com>
2026-02-24 14:50:16 +0530

    TOHID-AI

---
+++ plugins/ytsearch.js
@@ -0,0 +1,71 @@
+/*****************************************************************************
+ * Developed by Mr Tohid
+ * GitHub: https://github.com/Tohidkha6332
+ * WhatsApp: https://whatsapp.com/channel/0029VaGyP933bbVC7G0x0i2T
+ *
+ * © 2026 Tohidkhan6332 — Part of the TOHID-AI Project.
+ * Unauthorized copying or distribution is not allowed.
+ *****************************************************************************/
+
+
+const yts = require('yt-search');
+const settings = require('../settings');
+
+module.exports = {
+  command: 'ytsearch',
+  aliases: ['yts', 'playlist', 'playlista'],
+  category: 'music',
+  description: 'Search YouTube',
+  usage: '.yts [query]',
+
+  async handler(sock, message, args, context) {
+    const { chatId } = context;
+    const query = args.join(' ');
+    const prefix = settings.prefixes[0];
+
+    if (!query) {
+      return sock.sendMessage(chatId, { 
+        text: `Example: *${prefix}yts* Lil Peep` 
+      }, { quoted: message });
+    }
+
+    try {
+      await sock.sendMessage(chatId, { react: { text: '🔍', key: message.key } });
+
+      const result = await yts(query);
+      const videos = result.videos.slice(0, 10);
+
+      if (videos.length === 0) {
+        return sock.sendMessage(chatId, { text: '❌ No results found.' });
+      }
+
+      let searchText = `✨ *MUSIC SEARCH* ✨\n\n`;
+      
+      videos.forEach((v, index) => {
+        searchText += `*${index + 1}.🎧 ${v.title}*\n`;
+        searchText += `*⌚ Duration:* ${v.timestamp}\n`;
+        searchText += `*👀 Views:* ${v.views}\n`;
+        searchText += `*🔗 URL:* ${v.url}\n`;
+        searchText += `──────────────────\n`;
+      });
+
+      await sock.sendMessage(chatId, {
+        image: { url: videos[0].image },
+        caption: searchText
+      }, { quoted: message });
+
+    } catch (error) {
+      console.error('YouTube Search Error:', error);
+      await sock.sendMessage(chatId, { text: '❌ Error searching YouTube.' });
+    }
+  }
+};
+
+/*****************************************************************************
+ * Developed by Mr Tohid
+ * GitHub: https://github.com/Tohidkha6332
+ * WhatsApp: https://whatsapp.com/channel/0029VaGyP933bbVC7G0x0i2T
+ *
+ * © 2026 Tohidkhan6332 — Part of the TOHID-AI Project.
+ * Unauthorized copying or distribution is not allowed.
+ *****************************************************************************/
